b211c7ff377c0bdcbb8dff43c4115a3ee8d88a40
Sergei Mironov <ka6ah505@gmail.com>
2022-04-13 20:58:44 +0300

    rebased config to module core

--- app/db/database.py
+++ app/db/database.py
@@ -6,7 +6,7 @@ by: Mironov Sergei [ka6ah505@gmail.com]
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from config import BaseConfig
+from app.core.config import BaseConfig
 
 
 LINK_CONNECT_TO_BASE = f'postgresql+psycopg2://{BaseConfig.USER}:' \
