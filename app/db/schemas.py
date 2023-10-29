ae5b354bf11a81d6605a975c91f765bb83c3c095
Sergey Mironov <ka6ah505@gmail.com>
2023-10-29 19:56:14 +0300

    Удалил неиспользуемый DTO-класс. Убрал устаревший orm_mode

--- app/db/schemas.py
+++ app/db/schemas.py
@@ -14,16 +14,9 @@ class Record(BaseModel):
     close: float
     volume: int
     timeFrame: str
-    # id: int
-
-    class Config:
-        orm_mode = True
 
 
 class RecSt(BaseModel):
     id: int = None
     ticket: str
     close: float = None
-
-    class Config:
-        orm_mode = True
